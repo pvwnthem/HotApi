73ca55e1369d6b77798722a9002cc6dfff51d43c
pvwnthem <pvwn.them@gmail.com>
2022-12-24 00:05:00 -0500

    add app.use's

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,11 @@
 import express from 'express';
+import api from './routes/api';
 
 const app = express();
+
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/v1', api)
