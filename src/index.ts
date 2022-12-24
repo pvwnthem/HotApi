073d1eefad8972290ed54a189d0dd472fac2602f
pvwnthem <pvwn.them@gmail.com>
2022-12-24 00:10:42 -0500

    app listen on port

--- src/index.ts
+++ src/index.ts
@@ -9,3 +9,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use('/v1', api)
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}
+);
