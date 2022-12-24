cfa0fb7edc8b13d231a02ce09fdb826b6aba78c4
pvwnthem <pvwn.them@gmail.com>
2022-12-24 13:19:36 -0500

    add healthcheck endpoint

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -10,4 +10,20 @@ router.get("/", (req, res) => {
 
 
 
+router.get('/health', async (_req, res, _next) => {
+
+    const healthcheck = {
+        uptime: process.uptime(),
+        message: 'OK',
+        timestamp: Date.now()
+    };
+    try {
+        res.send(healthcheck);
+    } catch (error) {
+        healthcheck.message = error;
+        res.status(503).send();
+    }
+});
+
+
 export default router;
